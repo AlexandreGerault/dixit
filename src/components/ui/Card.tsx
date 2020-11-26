ba27ff45babf2ee171cffa6ebdcc6de9f44a5477
Alexandre Gérault <alexandre@hexium.io>
2020-11-26 21:24:01 +0100

    feat(style): style Card component

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import PropTypes, {InferProps} from 'prop-types'
+import {type} from 'os'
 
-type CardProps = {
-  cardId: string
-  playerName: string | undefined
-}
-
-const Card: React.FunctionComponent<CardProps> = ({
+function Card({
   cardId,
   playerName = '',
-}: CardProps) => {
+  isSelectedCard,
+  isGoodAnswer,
+}: InferProps<typeof Card.propTypes>) {
   return (
-    <div>
-      <img src={`./public/img/cards/${cardId}.png`} />
-      {playerName && <div>{playerName}</div>}
+    <div className="card">
+      <img
+        src={`img/cards/${cardId}`}
+        className={`card__img ${isSelectedCard ? 'card--selected' : ''} ${
+          isGoodAnswer ? 'card--good' : ''
+        }`}
+      />
+      {playerName && <div className="card__playerName">{playerName}</div>}
     </div>
   )
+}
+
+Card.propTypes = {
+  cardId: PropTypes.string.isRequired,
+  playerName: PropTypes.string,
+  isSelectedCard: PropTypes.bool,
+  isGoodAnswer: PropTypes.bool,
 }
 
 export {Card}
