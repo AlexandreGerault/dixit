95be33bc2cae316cd0932256c7fcb38ce69caa61
Alexandre Gérault <alexandre@hexium.io>
2020-11-26 20:44:49 +0100

    feat(tabs): tab styling

--- src/components/navigation/TabNav.tsx
+++ src/components/navigation/TabNav.tsx
@@ -6,17 +6,24 @@ function TabNav({children}: InferProps<typeof TabNav.propTypes>) {
   const {keys, activeTab, setActiveTab, tab} = useTabs(children)
 
   return (
-    <>
-      <ul>
-        {Array.isArray(keys) &&
-          keys.map(k => (
-            <li key={k}>
-              <button onClick={() => setActiveTab(k)}>{k}</button>
-            </li>
-          ))}
-      </ul>
-      {tab}
-    </>
+    <div className="tabs">
+      <nav className="tabs__header">
+        <ul>
+          {Array.isArray(keys) &&
+            keys.map(k => (
+              <li key={k}>
+                <button
+                  onClick={() => setActiveTab(k)}
+                  className={activeTab === k ? 'selected' : ''}
+                >
+                  {k}
+                </button>
+              </li>
+            ))}
+        </ul>
+      </nav>
+      <div className="tabs__content">{tab}</div>
+    </div>
   )
 }
 
